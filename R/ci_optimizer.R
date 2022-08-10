fc185654d4bf3291fc27b33a2a5d0f0efcc9bf8a
Olgun AYDIN <olgunaydinn@gmail.com>
2022-08-10 13:33:57 +0200

    Create ci_optimizer.R

---
+++ R/ci_optimizer.R
@@ -0,0 +1,25 @@
+ci_optimizer <- function(x)
+{
+  
+  d <- dim(x)
+  c_count <- d[2]
+  w <- matrix(NA, nrow = c_count,1)
+  
+  objective_function <- function(we)
+  {
+    ww<- we[1:3]
+        x_w <- x*ww
+    return(x_w)
+  }
+  
+  
+  x0 <- c(0.05, 0.05, 0.05)
+  #lb <- c(0,0,0,0,0)
+  #ub <- c(1,1,1,1,1)
+  #opt_w<- 
+  #nelder_mead(objective_function, x0, rep(-0.5, 3), rep(0.5, 3))
+  neldermead(x0, objective_function, rep(0, 3), rep(0.99, 3),nl.info = T)
+  #optim(x0, objective_function, method = "Nelder-Mead")
+  
+  #TODO: Test the code!
+}
