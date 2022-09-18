5548a5a4ec46ce59c2055e34557573107b5a2067
olgnaydn <olgunaydinn@gmail.com>
2022-09-18 22:47:31 +0200

    optimization solver implemented

--- R/ci_optimizer.R
+++ R/ci_optimizer.R
@@ -5,11 +5,11 @@ ci_optimizer <- function(x)
 
   d <- dim(x)
   c_count <- d[2]
-  w <- matrix(NA, nrow = c_count,1)
+#  w <- matrix(NA, nrow = c_count,1)
 
-  objective_function <- function(ww)
+  objective_function <- function(w_w)
   {
-       we <- as.matrix(ww)
+       we <- as.matrix(w_w)
        x_w <- as.matrix(x)%*%we
 
        #Si calculation - linear
@@ -18,35 +18,34 @@ ci_optimizer <- function(x)
 
        for (i in 1:d)
        {
-         m <- lm(y$ci~as.matrix(x[,i]))
+         m <- lm(x_w~as.matrix(x[,i]))
          m_s <- summary(m)
          r_2 <- m_s$r.squared
          s_i <- rbind(s_i,r_2)
        }
 
-    return(x_w)
+       w_d <- 1/d
+       s_d <- as.data.frame(rep(w_d,d))
+       s_i_norm <- s_i/sum(s_i)
+
+       s_d_mse <- sum((s_i - s_d)^2)
+
+    return(s_d_mse)
   }
 
-  #heq1 <- function(ww) sum(ww) - 1
+  heq1 <- function(w_w) sum(w_w) - 1
 
   x0 <- c(0.1666667, 0.1666667, 0.1666667,0.1666667,0.1666667,0.1666667)
-  #x0 <- c(0, 0, 0, 0, 0)
-  lb <- c(0.01,0.01,0.01,0.01,0.01,0.01)
-  ub <- c(1,1,1,1,1,1)
+  #x0 <- c(0, 0, 0, 0, 0, 0)
+  lb <- c(0.001,0.001,0.001,0.001,0.001,0.001)
+  #lb <- c(0,0,0,0,0,0)
+  ub <- c(0.99,0.99,0.99,0.99,0.99,0.99)
 
-  neldermead(x0, objective_function,lower=lb,upper=ub)
-  res <- fmincon(x0, objective_function, lb = lb, ub = ub,heq = heq1)
+  #neldermead(x0, objective_function,lower=lb,upper=ub,nl.info = T)
+  res <- fmincon(x0, objective_function, lb = lb, ub = ub, Aeq = Aeq2)
 
-  fminsearch(objective_function, x0, lower = lb, upper = ub,method = "Hooke-Jeeves")
-  optim(x0,objective_function,method = "Nelder-Mead")
+  #fminsearch(objective_function, x0, lower = lb, upper = ub,method = "Hooke-Jeeves")
+  #optim(x0,objective_function,method = "Nelder-Mead",lower = lb, upper = ub)
 
-  #TODO: Test the code!
-
-  # weights sum up to 1, positive.
-    # lb = 0.001
-    # ub = 1
-  # no threshold will be added for Si
-  # run optimization till normalized Si will be equal (or defined by the user - future functionality)
-    # Remove variables with highest Si till the moment that Sis are equal.
-
+  return(res)
 }
