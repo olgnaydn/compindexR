2a7be5373b573b499806db324c475fa07ab21981
olgnaydn <olgunaydinn@gmail.com>
2022-09-25 13:39:37 +0200

    tıdying up

--- R/ci_optimizer.R
+++ R/ci_optimizer.R
@@ -1,11 +1,5 @@
 ci_optimizer <- function(x)
 {
-  # WORK IN PROGRESS !!! ###
-
-
-  d <- dim(x)
-  c_count <- d[2]
-#  w <- matrix(NA, nrow = c_count,1)
 
   objective_function <- function(w_w)
   {
@@ -34,18 +28,10 @@ ci_optimizer <- function(x)
   }
 
   heq1 <- function(w_w) sum(w_w) - 1
-
+  x0 <- rep(1/d, d)
   x0 <- c(0.1666667, 0.1666667, 0.1666667,0.1666667,0.1666667,0.1666667)
-  #x0 <- c(0, 0, 0, 0, 0, 0)
   lb <- c(0.001,0.001,0.001,0.001,0.001,0.001)
-  #lb <- c(0,0,0,0,0,0)
-  ub <- c(0.99,0.99,0.99,0.99,0.99,0.99)
-
-  #neldermead(x0, objective_function,lower=lb,upper=ub,nl.info = T)
-  res <- fmincon(x0, objective_function, lb = lb, ub = ub, Aeq = Aeq2)
-
-  #fminsearch(objective_function, x0, lower = lb, upper = ub,method = "Hooke-Jeeves")
-  #optim(x0,objective_function,method = "Nelder-Mead",lower = lb, upper = ub)
-
+  ub <- c(0.999,0.999,0.999,0.999,0.999,0.999)
+  res <- fmincon(x0, objective_function, lb = lb, ub = ub, heq = Aeq1)
   return(res)
 }
