07bd5e81f3651d71767074d8a69464954322c3d6
pietrzakmichal <pietrzak.torun@gmail.com>
2022-09-29 11:32:58 +0200

    Update calc_compindex.R

--- R/calc_compindex.R
+++ R/calc_compindex.R
@@ -1,7 +1,7 @@
 calc_compindex <- function(x,avg_type = "simple",vif_threshold = 4.5)
 {
 
-  initial_si <- si_linear_exc_vif(x,avg_type = "simple",vif_threshold = 4.5)
+  initial_si <- si_linear_exc_vif(cel4_adjusted,avg_type = "simple",vif_threshold = 4.5)
   weight_opt <-ci_optimizer(x[,-c(initial_si$vif_index)])
 
   #TODO: Add Si calculation after optimization and also add while loop to run optimization
