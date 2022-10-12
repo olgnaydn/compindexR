453ec870b6c1714a91af7558bc179329cd520d74
olgnaydn <olgunaydinn@gmail.com>
2022-10-12 17:52:15 +0200

    new steps added to final calc

--- R/calc_compindex.R
+++ R/calc_compindex.R
@@ -1,16 +1,15 @@
-calc_compindex <- function(x,avg_type = "simple",vif_threshold = 4.5, si_diff = 0.1)
+calc_compindex <- function(x,avg_type = "simple",vif_threshold = NULL, si_diff = 0.1)
 {
 
-  initial_si <- si_linear_exc_vif(cel4_adjusted,avg_type = "simple",vif_threshold = 4.5)
-  weight_opt <-ci_optimizer(x[,-c(initial_si$vif_index)])
-
-  #TODO: Add Si calculation after optimization and also add while loop to run optimization
-  #gives equal Sis
-  #TODO: Round Si, they dont have sharp equal. Even still rounding will not work add some thresholding
-  # e.g. 2% different than avg Si
-  # Add possibility to choose standardization function for x.
-  # Calculate y after obtaining optimum weights then order(desc)
-  # Run optimization without taking VIF into account, user should be able to decide on this.
+  if(is.null(vif_threshold))
+  {
+    initial_si <- si_linear_exc(x,avg_type = avg_type)
+    we_opt_first_it <-ci_optimizer(x)
+  }else
+    {
+    initial_si <- si_linear_exc_vif(cel4_adjusted,avg_type = avg_type,vif_threshold = vif_threshold)
+    we_opt_first_it <-ci_optimizer(x[,-c(initial_si$vif_index)])
+  }
 
   # Controlling Si values for the optimization loop
   upper_threshold <- 1 + si_diff
